03b1c9dea57e2c34feca2fcf41d1cf0ed43deecd
iainkiloh <36046209+iainkiloh@users.noreply.github.com>
2018-02-01 17:09:27 +0000

    Add files via upload

---
+++ WebDiagnostics/App_Start/RouteConfig.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace WebDiagnostics
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+               null,
+               "",
+               new
+               {
+                   controller = "QueueMonitor",
+                   action = "Index",
+                   startDate = DateTime.Now.AddDays(-13).ToString("dd-MM-yyyy"),
+                   endDate = DateTime.Now.ToString("dd-MM-yyyy"),
+                   status = "All",
+                   page = 1
+               }
+               );
+
+            routes.MapRoute(
+                 name: "Default",
+                 url: "{controller}/{action}/{id}",
+                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+             );
+
+
+        }
+    }
+}
+
+
+
+ //routes.MapRoute(
+ //              null,
+ //              "{startDate}/{endDate}/{status}/{page}"
+ //               ,
+ //              new
+ //              {
+ //                  controller = "QueuedTasks",
+ //                  action = "Index"
+
+ //              },
+ //                  new { page = @"\d+" }
+ //              );
+           
+
+ //           routes.MapRoute(
+ //              null,
+ //              "{status}/{page}"
+ //               ,
+ //              new
+ //              {
+ //                  controller = "QueuedTasks",
+ //                  action = "Index",
+ //                  startDate = DateTime.Now.ToString("dd-MM-yyyy"),
+ //                  endDate = DateTime.Now.ToString("dd-MM-yyyy")
+ //              },
+ //                  new { page = @"\d+" }
+ //              );
+
+ //           routes.MapRoute(
+ //              null,
+ //              "{page}"
+ //               ,
+ //              new
+ //              {
+ //                  controller = "QueuedTasks",
+ //                  action = "Index",
+ //                  startDate = DateTime.Now.ToString("dd-MM-yyyy"),
+ //                  endDate = DateTime.Now.ToString("dd-MM-yyyy"),
+ //                  status = "All"
+ //              },
+ //                  new { page = @"\d+" }
+ //              );
+
+//routes.MapRoute(
+//    name: "Default",
+//    url: "{controller}/{action}/{id}",
+//    defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+//);
+
+
